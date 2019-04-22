8e1f3e4f70a4414af917ea1210b56f24b1058160
cyboryan <32955000+cyboryan@users.noreply.github.com>
2019-04-22 14:28:55 +0800

    Update easeform-updater20180422.php
    a

--- easeform-updater20180422.php
+++ easeform-updater20180422.php
@@ -1,7 +1,25 @@
-<div class="block">
-    <div class="box squircle">
-        <p class="center-text blue-text padding-vertical block text-24px">New Software Update Availale! Contact the Software Developer!</p>
-    </div>
-</div>
+<?php
+    $updateavailable = 1;
 
-<span class="span-30px"></span>
+    if ($updateavailable == 1) {
+    ?>
+    <div class="block">
+        <div class="box squircle">
+            <p class="center-text blue-text padding-vertical block text-24px">New Software Update Availale! Contact the Software Developer!</p>
+        </div>
+    </div>
+
+    <span class="span-30px"></span>
+    <?php ;
+    }
+    else {
+        if ($_SESSION['justopened'] == 1) {
+            require "res/inner/newsession.php";
+            $_SESSION['justopened'] = 0;
+        }
+        else {
+            echo "";
+        }
+    }
+?>
+
