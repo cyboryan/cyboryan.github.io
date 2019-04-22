bed574102246e8f1b7b180e5f5e3af5397a61f89
cyboryan <32955000+cyboryan@users.noreply.github.com>
2019-04-22 14:07:45 +0800

    Create easeform-updater20180422.php

---
+++ easeform-updater20180422.php
@@ -0,0 +1,7 @@
+<div class="block">
+    <div class="box squircle">
+        <p class="center-text blue-text padding-vertical block text-24px">New Software Update Availale! Contact the Software Developer!</p>
+    </div>
+</div>
+
+<span class="span-30px"></span>
